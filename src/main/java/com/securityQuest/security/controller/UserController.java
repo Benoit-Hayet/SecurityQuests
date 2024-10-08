5e5ac7c465d54e4018cc6059de3a2799d4591c36
Benoit-Hayet <hayetbenoit@gmail.com>
2024-10-08 17:17:09 +0200

    Ok

---
+++ src/main/java/com/securityQuest/security/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.securityQuest.security.controller;
+
+import com.securityQuest.security.model.User;
+import com.securityQuest.security.repository.UserRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    // Constructor injection for UserRepository
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userRepository.findAll();
+        if (users.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(users);
+    }
+}
